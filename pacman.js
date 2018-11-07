3dbdd2d75d6cc106d4cdab70df73672bb7d49543
fiol877 <fiol877@hotmail.com>
2018-11-07 19:49:19 +0100

    tasca 2 probablement inacabada.
    No estic del tot segur de que el canvi de direcció estigui ben fet.

--- pacman.js
+++ pacman.js
@@ -34,8 +34,13 @@ var mapa = [
 [0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0]
 ]
 
-layout();
-fantasmes();
+
+//layout();
+ghost1();
+ghost2();
+ghost3();
+jugador();
+
 
 
 
@@ -44,7 +49,7 @@ function layout(){
     var rows = "";
     for (var x=0;x<mapa.length;x++){
         for (var y=0;y<mapa[x].length;y++){
-            if (mapa[x][y]==1){
+            /*if (mapa[x][y]==1){
                 mapa[x][y] = "";
                 rows+=mapa[x][y] + " ";
             }
@@ -52,7 +57,7 @@ function layout(){
             else{
             mapa[x][y] = "";
             rows +=mapa[x][y] + "X";
-            }
+            }*/
 
             if (y==(mapa[x].length)-1){
                 rows+=mapa[x][y]+"\n";
@@ -67,27 +72,85 @@ function layout(){
 }
 
 
-
-function fantasmes(){
-    var rows="";
-    for(var i=1;i<=3;i++){
-        var avanza = false;
+function ghost1(){
+    var avanza = false;
         
         do{
             var x = Math.floor(Math.random() * mapa.length);
             var y = Math.floor(Math.random() * mapa[0].length);
+            direccioFant1 = Math.floor(Math.random() * (5-1)+1);
                 if (mapa[x][y]==1){
-                    mapa[x][y]="F";
+                    fantasma1 = mapa[x][y];
+                    mapa[x][y]="F1";
+                    avanza = true;
+                }
+        }while(avanza==false);
+    
+}
+
+function ghost2(){
+    var avanza = false;
+
+    do{
+            var x = Math.floor(Math.random() * mapa.length);
+            var y = Math.floor(Math.random() * mapa[0].length);
+            direccioFant2 = Math.floor(Math.random() * (5-1)+1);
+                if (mapa[x][y]==1){
+                    fantasma2 = mapa[x][y];
+                    mapa[x][y]="F2";
+                    avanza = true;
+                }
+        }while(avanza==false);
+    
+}
+ 
+function ghost3(){
+    var avanza = false;
+    do{
+            var x = Math.floor(Math.random() * mapa.length);
+            var y = Math.floor(Math.random() * mapa[0].length);
+            direccioFant3 = Math.floor(Math.random() * (5-1)+1);
+                if (mapa[x][y]==1){
+                    fantasma3 = mapa[x][y];
+                    mapa[x][y]="F3";
                     avanza = true;
                 }
         }while(avanza==false);
         
-        }
-    layout();
 }
 
 
 function jugador(){
     var posicio = null;
-    direccio = null;
+    var direccio = null;
+    
+    var avanza = false; 
+    
+    do{
+            var x = Math.floor(Math.random() * mapa.length);
+            var y = Math.floor(Math.random() * mapa[0].length);
+            direccioJugador = Math.floor(Math.random() * (5-1)+1);
+
+                if (mapa[x][y]==1){
+                    fantasma3 = mapa[x][y];
+                    mapa[x][y]="X";
+                    avanza = true;
+                }
+        }while(avanza==false);
+    layout();
 }
+
+
+
+var Up=1, right=2, down=3, left=4;
+/*
+function direccioFantasma(){
+    if (mapa[x][y]==1){
+        
+        do{
+            var x = Math.floor(Math.random() * mapa.length);
+            var y = Math.floor(Math.random() * mapa[0].length);
+            direccioFant = Math.floor(Math.random() * (5-1)+1);
+        }
+        
+}*/
